7d115b234b5d30a28e1e1f42ddb436c5144a531f
MarceloCLopes <marc01mcl@gmail.com>
2022-09-09 16:48:55 -0300

    created project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Effect button hover animation
+
+Animação de buttons em react usando vitejs com tailwindcss. Originalmente feito em html e css.
+
+#### tecnologias:
+
+- tailwindcss
+
+---
+
+## Screenshot
+
+![video1](./.github/video1.gif)
+
+---
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div id="wrapper">
+      <div className="w-56 h-11 leading-10 cursor-pointer mx-0 my-8 relative bor border-[3px] border-solid border-white before:content-[''] before:absolute before:w-full before:h-full before:top-[-50px] before:left-0 before:bg-white hover:before:top-0 before:transition-all before:ease-in before:delay-500 overflow-hidden">
+        <p className="relative z-[1] text-white uppercase text-center font-black transition-all ease-in delay-500 hover:text-[#00bfb6] hover:transition-all hover:ease-in">
+          hover over me
+        </p>
+      </div>
+
+      <div className="w-56 h-11 leading-10 cursor-pointer mx-0 my-8 relative bor border-[3px] border-solid border-white before:content-[''] before:absolute before:w-full before:h-full before:top-0 before:-left-56 before:bg-white hover:before:left-0 before:transition-all before:ease-in before:delay-500 overflow-hidden">
+        <p className="relative z-[1] text-white uppercase text-center font-black transition-all ease-in delay-500 hover:text-[#00bfb6] hover:transition-all hover:ease-in">
+          hover over me
+        </p>
+      </div>
+
+      <div className="w-56 h-11 leading-10 cursor-pointer mx-0 my-8 relative bor border-[3px] border-solid border-white before:content-[''] before:absolute before:w-full before:h-full before:top-0 before:left-56 before:bg-white hover:before:left-0 before:transition-all before:ease-in before:delay-500 overflow-hidden">
+        <p className="relative z-[1] text-white uppercase text-center font-black transition-all ease-in delay-500 hover:text-[#00bfb6] hover:transition-all hover:ease-in">
+          hover over me
+        </p>
+      </div>
+
+      <div className="w-56 h-11 leading-10 cursor-pointer mx-0 my-8 relative bor border-[3px] border-solid border-white before:content-[''] before:absolute before:w-full before:h-full before:top-[50px] before:left-0 before:bg-white hover:before:top-0 before:transition-all before:ease-in before:delay-500 overflow-hidden">
+        <p className="relative z-[1] text-white uppercase text-center font-black transition-all ease-in delay-500 hover:text-[#00bfb6] hover:transition-all hover:ease-in">
+          hover over me
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
